fc1789684bc36eff4e144a74589c1c380077eaeb
Giannina <95765874+GianninaGit@users.noreply.github.com>
2023-04-10 02:39:04 +0200

    Update Invoke command.js

--- Invoke command.js
+++ Invoke command.js
@@ -14,5 +14,24 @@ describe('Our first suite', () => {
         cy.get ('[for="exampleInputEmail1"]').then (label =>{
             expect(label.text()).to.equal('Email address')
         })
+          
+         //3a
+        cy.get ('[for="exampleInputEmail1"]').invoke('text'). then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        //3b
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            // no-> .find('span')
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            // .should('contain', "checked") -> Funciona! Otra forma es:
+            .then(classValue => {
+                expect(classValue).to.contain('checked')
+            })
+
+          
 
     })
